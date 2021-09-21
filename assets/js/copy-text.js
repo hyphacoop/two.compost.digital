49ab7586057c8d8acb9019dacab2bca4b3b4bdbc
Benedict Lau <b@deprecated.systems>
2021-09-20 18:06:46 -0700

    Fix annotation images in re-sister

--- assets/js/copy-text.js
+++ assets/js/copy-text.js
@@ -2,7 +2,7 @@ async function copyText(copyTextElementId, copyIconElementId, copiedButtonText, 
   try {
     navigator.clipboard.writeText(copiedClipboardText)
     document.getElementById(copyTextElementId).innerText = copiedButtonText
-    document.getElementById(copyIconElementId).src = "/images/done-icon.svg"
+    document.getElementById(copyIconElementId).src = "../images/done-icon.svg"
   } catch (e) {
     console.error('Failed to copy text to clipboard: ', e)
   }
--- assets/js/fetch-markdown.js
+++ assets/js/fetch-markdown.js
@@ -4,16 +4,16 @@ async function fetchMarkdown(markdownUrl) {
     if (markdownText !== undefined && markdownText.length > 0) {
       navigator.clipboard.writeText(markdownText)
       document.getElementById('copy-button-text').innerText = "Markdown copied"
-      document.getElementById('copy-icon').src = "/images/done-icon.svg"
+      document.getElementById('copy-icon').src = "../images/done-icon.svg"
     } else {
       document.getElementById('copy-error').innerText = "Failed to fetch markdown text."
       document.getElementById('copy-button-text').innerText = "Copy markdown"
-      document.getElementById('copy-icon').src = "/images/copy-icon.svg"
+      document.getElementById('copy-icon').src = "../images/copy-icon.svg"
     }
   } catch (e) {
     console.error('Failed to fetch markdown text: ', e)
     document.getElementById('copy-error').innerText = "Failed to fetch markdown text."
     document.getElementById('copy-button-text').innerText = "Copy markdown"
-    document.getElementById('copy-icon').src = "/images/copy-icon.svg"
+    document.getElementById('copy-icon').src = "../images/copy-icon.svg"
   }
 }
